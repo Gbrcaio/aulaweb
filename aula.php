c492701b50c7155a914eb0dd7e60ebbcad00ac79
Gabriel Caio <gabrielcaio.oc@gmail.com>
2025-11-14 21:11:43 -0300

    Add files via upload

---
+++ aula.php
@@ -0,0 +1,11 @@
+<?php
+//variáveis
+$variavel = "<b>Gabriel</b> ";
+$numero = 26;
+
+//print com concatenação
+echo "<i>Hello World! </i> <br>Welcome! " . 
+    $variavel . "seu numero da sorte é " . 
+    $numero;
+
+?>
---
+++ trataformulario.php
@@ -0,0 +1,9 @@
+<?php
+    echo "O Produto <i> " . $_GET['produto'] . 
+    "</i> tem " . $_GET['quantidade'] . 
+    " quantidades em estoque " . 
+    " com o preço de " . $_GET['preco']  .
+    " reais. ";
+
+
+?>
